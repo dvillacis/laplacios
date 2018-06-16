b5f499da12953044d8e295de1e75a68de09313fe
David Villacis <david.villacis01@epn.edu.ec>
2018-06-16 09:10:12 -0500

    Added database backup file

---
+++ backup_database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Database credentials
+user="laplacios2018"
+password=""
+host=""
+db_name="FIFA2018"
+
+# Other options
+date=$(date +"%Y-%m-%d_%H_%M_%S")
+
+# Set default file permissions
+umask 177
+
+# Dump database into SQL file
+mysqldump --user=$user --host=$host $db_name > $db_name-$date.sql
