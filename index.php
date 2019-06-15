76ccee0b99786c4c38724bfad686c02e3467bf53
David Villacis <david.villacis01@epn.edu.ec>
2019-06-14 23:43:37 -0500

    Modified login

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 include('login.php'); // Includes Login Script
 if(isset($_SESSION['login_user'])){
-header("location: profile.php");
+	header("location: profile.php");
 }
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Banco Central del Laplacio</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
-    <!-- Stylesheets
+<head>
+	<title>Banco Central del Laplacio</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<!-- Stylesheets
 	============================================= -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
@@ -21,8 +21,8 @@ header("location: profile.php");
 
 	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-  </head>
-  <body class="stretched">
+</head>
+<body class="stretched">
 
 	<!-- Document Wrapper
 	============================================= -->
@@ -37,14 +37,14 @@ header("location: profile.php");
 				<div class="section nopadding nomargin" style="width: 100%; height: 100%; position: absolute; left: 0; top: 0; background: #444;"></div>
 
 				<div class="section nobg full-screen nopadding nomargin">
-					<div class="container-fluid vertical-middle divcenter clearfix">
+					<div id="main" class="container-fluid vertical-middle divcenter clearfix">
 
 						<div class="center">
 							<h1>Banco Central Laplacio</h1>
 						</div>
 
 						<div class="card divcenter noradius noborder" style="max-width: 400px;">
-							<div class="card-body" style="padding: 40px;">
+							<div id="login" class="card-body" style="padding: 40px;">
 								<form id="login-form" name="login-form" class="nobottommargin" action="" method="post">
 									<h3>Login to your Account</h3>
 
@@ -59,8 +59,9 @@ header("location: profile.php");
 									</div>
 
 									<div class="col_full nobottommargin">
-										<button class="button button-3d button-black nomargin" id="login-form-submit" name="login-form-submit" value="login">Login</button>
+										<button class="button button-3d button-black nomargin" id="login-form-submit" name="submit" value=" Login ">Login</button>
 									</div>
+									<span><?php echo $error; ?></span>
 								</form>
 							</div>
 						</div>
@@ -87,20 +88,20 @@ header("location: profile.php");
 	<script src="js/functions.js"></script>
 
 </body>
-  <body>
-    <div id="main">
-    <h1>Banco Central del Laplacio</h1>
-    <div id="login">
-        <h2>LOGIN</h2>
-        <form action="" method="post">
-            <label>UserName :</label>
-            <input id="name" name="username" placeholder="username" type="text">
-            <label>Password :</label>
-            <input id="password" name="password" placeholder="**********" type="password">
-            <input name="submit" type="submit" value=" Login ">
-            <span><?php echo $error; ?></span>
-        </form>
-    </div>
-    </div>
-  </body>
+<body>
+	<div id="main">
+		<h1>Banco Central del Laplacio</h1>
+		<div id="login">
+			<h2>LOGIN</h2>
+			<form action="" method="post">
+				<label>UserName :</label>
+				<input id="name" name="username" placeholder="username" type="text">
+				<label>Password :</label>
+				<input id="password" name="password" placeholder="**********" type="password">
+				<input name="submit" type="submit" value=" Login ">
+				<span><?php echo $error; ?></span>
+			</form>
+		</div>
+	</div>
+</body>
 </html>
