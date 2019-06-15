09b057a9ffe974d17dff1a873e63e8a6426ea0ee
David Villacis <david.villacis01@epn.edu.ec>
2019-06-15 00:09:43 -0500

    Changing profile

--- profile.php
+++ profile.php
@@ -52,10 +52,9 @@ include('session.php');
 									<div class="tabs tabs-alt clearfix" id="tabs-profile">
 
 										<ul class="tab-nav clearfix">
-											<li><a href="#tab-feeds"><i class="icon-rss2"></i> Feeds</a></li>
-											<li><a href="#tab-posts"><i class="icon-pencil2"></i> Posts</a></li>
-											<li><a href="#tab-replies"><i class="icon-reply"></i> Replies</a></li>
-											<li><a href="#tab-connections"><i class="icon-users"></i> Connections</a></li>
+											<li><a href="#tab-feeds"><i class="icon-rss2"></i> Enviar Apuesta</a></li>
+											<li><a href="#tab-posts"><i class="icon-pencil2"></i> Mis Apuestas </a></li>
+											<li><a href="#tab-replies"><i class="icon-reply"></i> Tabla de Posiciones</a></li>
 										</ul>
 
 										<div class="tab-container">
@@ -250,111 +249,6 @@ include('session.php');
 												</ol>
 
 											</div>
-											<div class="tab-content clearfix" id="tab-connections">
-
-												<div class="row topmargin-sm">
-													<div class="col-lg-3 col-md-6 bottommargin">
-
-														<div class="team">
-															<div class="team-image">
-																<img src="images/team/3.jpg" alt="John Doe">
-															</div>
-															<div class="team-desc">
-																<div class="team-title"><h4>John Doe</h4><span>CEO</span></div>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-																	<i class="icon-facebook"></i>
-																	<i class="icon-facebook"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-																	<i class="icon-twitter"></i>
-																	<i class="icon-twitter"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-																	<i class="icon-gplus"></i>
-																	<i class="icon-gplus"></i>
-																</a>
-															</div>
-														</div>
-
-													</div>
-
-													<div class="col-lg-3 col-md-6 bottommargin">
-
-														<div class="team">
-															<div class="team-image">
-																<img src="images/team/2.jpg" alt="Josh Clark">
-															</div>
-															<div class="team-desc">
-																<div class="team-title"><h4>Josh Clark</h4><span>Co-Founder</span></div>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-																	<i class="icon-facebook"></i>
-																	<i class="icon-facebook"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-																	<i class="icon-twitter"></i>
-																	<i class="icon-twitter"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-																	<i class="icon-gplus"></i>
-																	<i class="icon-gplus"></i>
-																</a>
-															</div>
-														</div>
-
-													</div>
-
-													<div class="col-lg-3 col-md-6 bottommargin">
-
-														<div class="team">
-															<div class="team-image">
-																<img src="images/team/8.jpg" alt="Mary Jane">
-															</div>
-															<div class="team-desc">
-																<div class="team-title"><h4>Mary Jane</h4><span>Sales</span></div>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-																	<i class="icon-facebook"></i>
-																	<i class="icon-facebook"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-																	<i class="icon-twitter"></i>
-																	<i class="icon-twitter"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-																	<i class="icon-gplus"></i>
-																	<i class="icon-gplus"></i>
-																</a>
-															</div>
-														</div>
-
-													</div>
-
-													<div class="col-lg-3 col-md-6">
-
-														<div class="team">
-															<div class="team-image">
-																<img src="images/team/4.jpg" alt="Nix Maxwell">
-															</div>
-															<div class="team-desc">
-																<div class="team-title"><h4>Nix Maxwell</h4><span>Support</span></div>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-																	<i class="icon-facebook"></i>
-																	<i class="icon-facebook"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-																	<i class="icon-twitter"></i>
-																	<i class="icon-twitter"></i>
-																</a>
-																<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-																	<i class="icon-gplus"></i>
-																	<i class="icon-gplus"></i>
-																</a>
-															</div>
-														</div>
-
-													</div>
-												</div>
-
-											</div>
 
 										</div>
 
@@ -395,7 +289,8 @@ include('session.php');
 			});
 		</script>
 </body>
-<body>
+</html>
+<!-- <body>
   <div id="profile">
     <b id="welcome">Hola : <i><?php echo $login_session;?></i></b><br/>
     <b>Tienes : <i><?php echo $login_saldo;?></i> laplacios.</b><br/>
@@ -499,4 +394,4 @@ include('session.php');
         </tbody>
       </table>
     </div>
-  </body>
+  </body> -->
