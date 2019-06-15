93699df929a92ebced4842c02e1b2136efecb751
David Villacis <davidvvillacis.net>
2019-06-15 06:20:51 +0000

    modified backup

--- backup_database.sh
+++ backup_database.sh
@@ -4,7 +4,7 @@
 user="laplacios2018"
 password=""
 host=""
-db_name="FIFA2018"
+db_name="FIFA2018_BACK"
 
 # Other options
 date=$(date +"%Y-%m-%d_%H_%M_%S")
@@ -13,4 +13,4 @@ date=$(date +"%Y-%m-%d_%H_%M_%S")
 umask 177
 
 # Dump database into SQL file
-mysqldump --user=$user --host=$host $db_name > backups/$db_name-$date.sql
+mysqldump --user=$user --host=$host $db_name > backups/$db_name-$date-back.sql
