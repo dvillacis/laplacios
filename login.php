40cf0451cb84b83d3d6c8d179056850567763ca2
David Villacis <david.villacis01@epn.edu.ec>
2019-06-15 22:28:09 -0500

    Changing admin

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
         $username = mysql_real_escape_string($username);
         $password = mysql_real_escape_string($password);
         // Selecting Database
-        $db = mysql_select_db("FIFA2018_BACK", $connection);
+        $db = mysql_select_db("COPA_AMERICA_2019", $connection);
         // SQL query to fetch information of registerd users and finds user match.
         $query = mysql_query("select id_participante,nombre,saldo from participantes where passwd='$password' AND nombre='$username'", $connection);
         $rows = mysql_num_rows($query);
